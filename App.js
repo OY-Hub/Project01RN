87dcb3aeecc5991c7b80b870c4a7f84d7fa62243
O. Yassin <Ouichou.IT@gmail.com>
2018-04-02 14:14:42 +0100

    Update

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome Yassin to React Native!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
